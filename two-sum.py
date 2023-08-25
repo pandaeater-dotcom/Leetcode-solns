c6ad6f7ecea632ac90c4b1abdba0bd4e7d0a72be
Rohit Bharadwaj <65730671+pandaeater-dotcom@users.noreply.github.com>
2023-08-25 04:19:19 -0400

    Delete two-sum.py

--- two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashTable = {}
-        for x in range(len(nums)):
-            val = nums[x]
-            if target - val in hashTable:
-                return [nums.index(target - val), x]
-            if val not in hashTable:
-                hashTable[val] = target - val
