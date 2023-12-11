96db67abae2a8a17a5cd60db2612e24ff72dfa2f
Rohit Bharadwaj <65730671+pandaeater-dotcom@users.noreply.github.com>
2023-12-11 05:12:49 -0500

    Update 3sum.cc

--- 3sum.cc
+++ 3sum.cc
@@ -1,75 +1,33 @@
 class Solution {
-public:
-    void mergeSort(vector<int>& A, int l, int r) {
-        if (l >= r)
-            return;
-        int m = floor((l + r)/2);
-        mergeSort(A, l, m);
-        mergeSort(A, m + 1, r);
-        merge(A, l, m, r);
-    }
-    void merge(std::vector<int>& arr, int left, int middle, int right) {
-        int n1 = middle - left + 1;
-        int n2 = right - middle;
-
-        std::vector<int> leftArr(n1);
-        std::vector<int> rightArr(n2);
-
-        for (int i = 0; i < n1; ++i)
-            leftArr[i] = arr[left + i];
-        for (int j = 0; j < n2; ++j)
-            rightArr[j] = arr[middle + 1 + j];
-
-        int i = 0, j = 0, k = left;
-
-        while (i < n1 && j < n2) {
-            if (leftArr[i] <= rightArr[j]) {
-                arr[k] = leftArr[i];
-                ++i;
-            } else {
-                arr[k] = rightArr[j];
-                ++j;
-            }
-            ++k;
-        }
-
-        while (i < n1) {
-            arr[k] = leftArr[i];
-            ++i;
-            ++k;
-        }
-
-        while (j < n2) {
-            arr[k] = rightArr[j];
-            ++j;
-            ++k;
-        }
-    }
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
+    public:
+        vector<vector<int>> threeSum(vector<int>& nums) {
         set<vector<int>> triplets;
-        for (int i = 0; i < nums.size(); i++)
+        vector<vector<int>> tripletsVector;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < nums.size(); i++)
         {
-            int j = i + 1;
-            int k = nums.size() - 1;
-            while (j < k)
+            int l = i + 1;
+            int r = nums.size() - 1;
+            int target = -nums.at(i);
+            while (l < r)
             {
-                int sum = nums.at(i) + nums.at(j) + nums.at(k);
-                if (sum == 0)
+                int left = nums.at(l);
+                int right = nums.at(r);
+                if (left + right > target)
+                    r--;
+                else if (left + right < target)
+                    l++;
+                else
                 {
-                    triplets.insert({nums.at(i), nums.at(j), nums.at(k)});
-                    j++;
-                    k--;
+                    if (triplets.insert({nums.at(i), left, right}).second)
+                        tripletsVector.push_back({nums.at(i), left, right});
+                    r--;
+                    l++;
                 }
-                else if (sum > 0)
-                    k--;
-                else if (sum < 0)
-                    j++;
             }
         }
-        vector<vector<int>> tripletsVector;
-        for (auto it : triplets)
-            tripletsVector.push_back(it);
         return tripletsVector;
     }
 };
