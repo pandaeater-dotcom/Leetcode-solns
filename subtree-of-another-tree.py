a6b60a4da87a38cff4b14f2d7b87b85ebd68fcad
Rohit Bharadwaj <65730671+pandaeater-dotcom@users.noreply.github.com>
2024-01-23 23:03:28 -0500

    Create subtree-of-another-tree.py

---
+++ subtree-of-another-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and q or not q and p:
+            return False
+        if not p and not q:
+            return True
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root and not subRoot:
+            return True
+        elif not root and subRoot:
+            return False
+        elif root and not subRoot:
+            return True
+        return self.isSameTree(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
