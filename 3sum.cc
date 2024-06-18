05d8798ff1b49c4b78b5612248ac7d250d6799ba
Rohit Bharadwaj <65730671+pandaeater-dotcom@users.noreply.github.com>
2024-06-18 01:27:26 -0400

    Update 3sum.cc

--- 3sum.cc
+++ 3sum.cc
@@ -1,33 +1,27 @@
 class Solution {
-    public:
-        vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> triplets;
-        vector<vector<int>> tripletsVector;
-
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
+        vector<vector<int>> solns;
 
-        for(int i = 0; i < nums.size(); i++)
-        {
+        for (int i = 0; i < nums.size() - 2; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             int l = i + 1;
             int r = nums.size() - 1;
-            int target = -nums.at(i);
-            while (l < r)
-            {
-                int left = nums.at(l);
-                int right = nums.at(r);
-                if (left + right > target)
-                    r--;
-                else if (left + right < target)
-                    l++;
-                else
-                {
-                    if (triplets.insert({nums.at(i), left, right}).second)
-                        tripletsVector.push_back({nums.at(i), left, right});
-                    r--;
-                    l++;
+            while (l < r) {
+                int sum = nums[l] + nums[r] +nums[i];
+                if (sum > 0) r--;
+                else if (sum < 0) l++;
+                else {
+                    solns.push_back({nums[i], nums[l], nums[r]});
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+                    l++; r--;
                 }
             }
         }
-        return tripletsVector;
+
+        return solns;
     }
 };
