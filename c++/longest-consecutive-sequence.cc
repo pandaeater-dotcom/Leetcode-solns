eca5ec0125b057c4a3c7731fff318269e25ab360
Rohit Bharadwaj <65730671+pandaeater-dotcom@users.noreply.github.com>
2024-09-12 12:20:10 -0400

    Update longest-consecutive-sequence.cc

--- c++/longest-consecutive-sequence.cc
+++ c++/longest-consecutive-sequence.cc
@@ -1,56 +1,19 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if (nums.empty())
-            return 0;
-        else if (nums.size() == 1)
-            return 1;
-
-        int maxSequence = 1;
-        unordered_map<int, bool> isPresent;
-
-        for (const auto& num : nums)
-            isPresent[num] = true;
-        for (const auto& num : nums)
-            if (isPresent.find(num + 1) == isPresent.end() || isPresent.find(num - 1) != isPresent.end())
-                isPresent[num] = false;
-
-        for (const auto& [key, val] : isPresent)
-        {
-            if (!val)
-                continue;
-            int sequence = 0;
-            int elem = key;
-            while (isPresent.find(elem) != isPresent.end())
-            {
-                sequence++;
-                elem++;
-            }
-            if (sequence > maxSequence)
-                maxSequence = sequence;
-        }
-        return maxSequence;
-    }
-};
-
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
         if (nums.size() <= 1) return nums.size();
         unordered_set<int> nums_set;
 
         int maxSeqCt = 1;
         for (const auto& num : nums) nums_set.insert(num);
 
-        for (const auto& num : nums) {
-            if (nums_set.find(num - 1) != nums_set.end()) continue;
-            int it = num + 1;
+        for (const auto& num : nums_set) {
+            if (nums_set.contains(num - 1)) continue;
             int seqCt = 1;
-            while (nums_set.find(it) != nums_set.end()) {
+            while (nums_set.contains(num + seqCt)) {
                 seqCt++;
-                it++;
             }
-            if (seqCt > maxSeqCt) maxSeqCt = seqCt;
+            maxSeqCt = max(seqCt, maxSeqCt);
         }
         return maxSeqCt;
     }
