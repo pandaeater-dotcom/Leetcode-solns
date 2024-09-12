77f49350b3a3d4373d9b21b6eeb50e8306305360
Rohit Bharadwaj <65730671+pandaeater-dotcom@users.noreply.github.com>
2024-09-12 11:55:45 -0400

    Update two-sum-ii-input-array-is-sorted.cc

--- c++/two-sum-ii-input-array-is-sorted.cc
+++ c++/two-sum-ii-input-array-is-sorted.cc
@@ -21,8 +21,9 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         unordered_map<int, int> hashmap;
         for (int i = 0; i < numbers.size(); i++) {
-            if (hashmap.find(numbers[i]) != hashmap.end()) {
-                return {hashmap[numbers[i]] + 1, i + 1};
+            auto it = hashmap.find(numbers[i]);
+            if (it != hashmap.end()) {
+                return {it->second + 1, i + 1};
             }
             hashmap[target - numbers[i]] = i;
         }
