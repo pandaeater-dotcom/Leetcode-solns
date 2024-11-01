e5a17d0ab7831653dbb59642f416bae69a21e917
Rohit Bharadwaj <65730671+pandaeater-dotcom@users.noreply.github.com>
2024-11-01 13:32:11 -0300

    Update implement-trie-prefix-tree.cc

--- c++/implement-trie-prefix-tree.cc
+++ c++/implement-trie-prefix-tree.cc
@@ -1,56 +1,36 @@
 class Trie {
 private:
-  vector<Trie*> children;
+  array<Trie*, 26> children;
   bool isWord;
-  string str;
 public:
-  Trie(): isWord{false}, str{""} {}
-
-  Trie(char ch): str{ch}, isWord{false} {}
+  Trie(): isWord{false} {
+    children.fill(nullptr);
+  }
   
   void insert(string word) {
     if (word == "") {
       isWord = true;
       return;
     }
-    for (auto& child : children) {
-      string res = child->getChar();
-      if (res.length() && res[0] == word[0]) {
-        child->insert(word.substr(1));
-        return;
-      }
-    }
-    children.push_back(new Trie(word[0]));
-    children.back()->insert(word.substr(1));
+    int index = word[0] - 'a';
+    if (!children[index]) children[index] = new Trie();
+    children[index]->insert(word.substr(1));
   }
   
   bool search(string word) {
-    
     if (word == "") {
       if (isWord) return true;
       return false;
     }
-    for (const auto& child : children) {
-      string res = child->getChar();
-      if (res.length() && res[0] == word[0]) {
-        return child->search(word.substr(1));
-      }
-    }
-    return false;
+    int index = word[0] - 'a';
+    if (!children[index]) return false;
+    return children[index]->search(word.substr(1));
   }
   
   bool startsWith(string prefix) {
     if (prefix == "") return true;
-    for (const auto& child : children) {
-      string res = child->getChar();
-      if (res.length() && res[0] == prefix[0]) {
-        return child->startsWith(prefix.substr(1));
-      }
-    }
-    return false;
-  }
-
-  string getChar() {
-    return str;
+    int index = prefix[0] - 'a';
+    if (!children[index]) return false;
+    return children[index]->startsWith(prefix.substr(1));
   }
 };
