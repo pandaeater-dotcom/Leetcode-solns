b8e81ca77c67725f69a09ad6843df44708fd8760
Rohit Bharadwaj <65730671+pandaeater-dotcom@users.noreply.github.com>
2025-11-03 07:05:58 -0400

    Refactor merge function to handle intervals more efficiently

--- python/merge-intervals.py
+++ python/merge-intervals.py
@@ -1,9 +1,9 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key= lambda x: x[0])
-        mergedIntervals = [intervals[0]]
-        for interval in intervals[1:]:
-            if mergedIntervals[-1][1] < interval[0]:
+        intervals.sort(key=lambda x: x[0])
+        mergedIntervals = []
+        for interval in intervals:
+            if not mergedIntervals or mergedIntervals[-1][1] < interval[0]:
                 mergedIntervals.append(interval)
             else:
                 mergedIntervals[-1][1] = max(mergedIntervals[-1][1], interval[1])
